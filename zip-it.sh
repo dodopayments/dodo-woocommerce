31973cd6b660fafc4800f4f266dcca83809b4fe9
Ambuj Kumar <ambujs89@gmail.com>
2025-06-26 14:52:28 +0530

    chore: exclude extra directories to be included in zip file

--- zip-it.sh
+++ zip-it.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -r dodo-payments-for-woocommerce.zip . -x ".git/*" ".github/*"
+zip -r dodo-payments-for-woocommerce.zip . -x ".git/*" ".github/*" "zip-it.sh" ".gitignore" ".cursor/*" "dodo-payments-for-woocommerce.zip"
 
